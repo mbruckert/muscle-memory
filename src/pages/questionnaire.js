ad1bada359973ed196849e3adcd0946433a38001
Yousef Osman <osman.yousef6933@knights.ucf.edu>
2023-03-26 08:52:51 -0400

    changes made again.

--- src/pages/questionnaire.js
+++ src/pages/questionnaire.js
@@ -1,5 +1,4 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { Virtuoso } from 'react-virtuoso';
 import './questionnaire.css';
 
 function Questionnaire() {
