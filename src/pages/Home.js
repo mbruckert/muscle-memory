4d87add307ffc90598da969cad7802480c3947b7
Sabrina-Lopez <32684344+Sabrina-Lopez@users.noreply.github.com>
2023-03-26 10:23:29 -0400

    changes to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,43 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, getAssetPath, IonFooter, IonInput, IonTab, IonFab, IonIcon, IonButton } from '@ionic/react';
+import { useState } from 'react';
 import './Home.css';
 
 function Home() {
+
+  const [inputMessage, setInputMessage] = useState("");
+  
   return (
 
     <IonPage>
+
       <IonContent fullscreen>
+
         <IonToolbar>
+
           <IonTitle> Muscle Memory </IonTitle>
+
             </IonToolbar>
 
               <div className='chat'>
         
                 <div className='messageBox'>
           
-                    <ion-item>
-                  <ion-input placeholder="Muscle Memory"></ion-input>
-                  <IonButton shape="round">send</IonButton>
+                  <ion-item>
+
+                      <ion-input mbsc-input type = "inputMessage" placeholder="Enter Message Here" value = {inputMessage} onChange = {(e) => {
+                        setInputMessage(e.target.value)
+                      }}></ion-input>
+                      
+                      <IonButton shape="round">Send</IonButton>
+
                 </ion-item>
 
                 </div>
 
               </div>
+
             </IonContent>
+
           </IonPage>
 
     );
