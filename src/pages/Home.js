d9d2e618cd12a7973f948be674efa32ab8b47d73
Sabrina-Lopez <32684344+Sabrina-Lopez@users.noreply.github.com>
2023-03-26 04:04:11 -0400

    new home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,67 +5,9 @@ function Home() {
   return (
 
     <IonPage>
-
-      <IonHeader>
-
-        <IonToolbar>
-
-          <IonTitle> Muscle Memory </IonTitle>
-        </IonToolbar>
-
-      </IonHeader>
-
-      <IonContent fullscreen>
-
-        <IonHeader collapse="condense">
-
-          <IonToolbar>
-
-            <IonTitle size="large"> Muscle Memory </IonTitle>
-
-          </IonToolbar>
-
-        </IonHeader>
-
-        <div className = 'chat'>
-
-          <div className = 'messageBox'>
-
-            <IonToolbar>
-
-              <IonFooter>
-
-                <ion-item>
-
-                  <ion-label></ion-label>
-                  
-                  <ion-input placeholder="Muscle Memory Chat"></ion-input>
-                  
-                  <ion-fab>
-
-                    <ion-fab-button size="small">
-
-                      <ion-icon name="add"></ion-icon>
-
-                    </ion-fab-button>
-
-                  </ion-fab>
-
-                </ion-item>
-
-              </IonFooter>
-
-            </IonToolbar>
-
-          </div>
-
-        </div>
-
-      </IonContent>
-
     </IonPage>
 
-  );
+    );
 };
 
 export default Home;
