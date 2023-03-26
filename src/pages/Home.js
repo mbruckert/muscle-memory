f43465dbefe906c11b02f1d15a35641e5b9ec277
Yousef Osman <osman.yousef6933@knights.ucf.edu>
2023-03-26 12:00:39 -0400

    New home.js/css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,43 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, getAssetPath, IonFooter, IonInput, IonTab, IonFab, IonIcon, IonButton } from '@ionic/react';
+import { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter, IonInput, IonTab, IonFab, IonIcon, IonButton } from '@ionic/react';
 import './Home.css';
 
 function Home() {
-  return (
+  const [messages, setMessages] = useState([]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const messageInput = e.target.elements.messageInput.value;
+    if (messageInput) {
+      setMessages(prevMessages => [...prevMessages, messageInput]);
+      e.target.elements.messageInput.value = '';
+    }
+  }
+
+  return (
     <IonPage>
       <IonContent fullscreen>
         <IonToolbar>
           <IonTitle> Muscle Memory </IonTitle>
-            </IonToolbar>
-
-              <div className='chat'>
-        
-                <div className='messageBox'>
-          
-                    <ion-item>
-                  <ion-input placeholder="Muscle Memory"></ion-input>
-                  <IonButton shape="round">send</IonButton>
-                </ion-item>
-
+        </IonToolbar>
+        <div className='chat'>
+          <div className='messageBox'>
+            {messages.map((message, index) => {
+              return (
+                <div key={index} className='message'>
+                  {message}
                 </div>
-
-              </div>
-            </IonContent>
-          </IonPage>
-
-    );
+              )
+            })}
+          </div>
+          <form onSubmit={handleSubmit} className='input-box'>
+            <IonInput name='messageInput' placeholder='Muscle Memory' className='input' />
+            <IonButton type='submit' className='submit-button' shape='round'>Send</IonButton>
+          </form>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Home;
