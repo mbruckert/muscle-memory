7f0cc02ea61171368b99504c80352df82113d16a
Sabrina-Lopez <32684344+Sabrina-Lopez@users.noreply.github.com>
2023-03-26 06:27:32 -0400

    signup & login pg changes

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonList, IonItem } from '@ionic/react';
 import './login.css';
 
 function Login() {
@@ -9,22 +8,41 @@ function Login() {
       <div className = "flex">
 
         <IonContent fullscreen>
-
+        
           <div className = "Title"> 
 
-            <IonTitle> Muscle Memory </IonTitle>
+        <IonTitle> Muscle Memory </IonTitle>
 
-          </div>
+      </div>
 
-          <div>
-          
-            
+          <div className = "loginBox">
+<IonList>
+<IonItem>
 
-          </div>
+  <label>
+    
+    <input mbsc-input type = "text" data-label = "Email" placeholder = "Enter Email" />
+  
+  </label>
+
+</IonItem>
+
+<IonItem>
+
+  <label>
+
+      <input mbsc-input type = "password" data-label = "Password" placeholder = "Enter Password" data-start-icon="lock2" data-password-toggle = "true"/>
+  
+  </label>
+
+</IonItem>
+</IonList>
+
+</div>
           
           <div className = "loginButton">
 
-            <IonButton color = {'ion-color-warning-contrast'}> Login </IonButton>
+            <IonButton> Login </IonButton>
             
           </div>
 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,12 +1,22 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './signup.css';
 
 function Signup() {
   return (
 
-
     <IonPage>
+
+      <div className = "flex">
+
+          <div className = "Title"> 
+
+        <IonTitle> Muscle Memory </IonTitle>
+
+      </div>
+      </div>
+
+
+
     </IonPage>
 
     );
